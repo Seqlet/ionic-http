00654ad24661b2c475a6a7e6123aaf50e1e3f506
Non Eks <ratchanoneks@gmail.com>
2019-11-08 14:04:38 +0700

    fix :fuck god

--- src/models/score.model.ts
+++ src/models/score.model.ts
@@ -1,8 +1,10 @@
 export interface Score {
   id: number;
-  score: {
-    quiz1: number;
-    quiz2: number;
-    quiz3: number;
-  };
+  quiz1: number;
+  quiz2: number;
+  quiz3: number;
+  quiz4: number;
+  quiz5: number;
+  sum: number;
+
 }
--- src/providers/score.ts
+++ src/providers/score.ts
@@ -4,7 +4,7 @@ import { Score } from '../models/score.model';
 
 @Injectable()
 export class ScoreProvider {
-  private scoreURL = 'https://cors-anywhere.herokuapp.com/https://testflaskise2019.herokuapp.com/testjson?id=';
+  private scoreURL = 'https://cors-anywhere.herokuapp.com/http://labfk7.herokuapp.com/findscore?id=';
   constructor(public http: HttpClient) {}
   getScore(id: string) {
     return this.http.get<Score>(this.scoreURL + id);
